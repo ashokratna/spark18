5620dfacaa9934b7250a186cb1ef39f6cc1e0498
Ankur Pandey <ap622098@gmail.com>
2020-05-05 19:58:29 +0530

    Updates

---
+++ precache-manifest.d32d19365d218fa5363aed94fea5dc3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5c9f6bbfe4f97311ad8cee6bdce73b",
+    "url": "/spark18/index.html"
+  },
+  {
+    "revision": "2f58631cf8b5717a9cb3",
+    "url": "/spark18/static/css/2.3be8d482.chunk.css"
+  },
+  {
+    "revision": "7d40ace945dd62a81e1e",
+    "url": "/spark18/static/css/main.06e051e1.chunk.css"
+  },
+  {
+    "revision": "2f58631cf8b5717a9cb3",
+    "url": "/spark18/static/js/2.d060cae1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/spark18/static/js/2.d060cae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d40ace945dd62a81e1e",
+    "url": "/spark18/static/js/main.c0fc6c9b.chunk.js"
+  },
+  {
+    "revision": "a234f2b384bb5d603066",
+    "url": "/spark18/static/js/runtime-main.9835d790.js"
+  }
+]);
